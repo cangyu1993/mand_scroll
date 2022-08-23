192e071c0a100c341f075f11abe7ad8f74af387f
cangyu1993 <1765244720@qq.com>
2022-08-23 18:30:11 +0800

    提交

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,69 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <md-scroll-view ref="scrollView" @scroll="$_onScroll">
+      <div class="head_desc">头图</div>
+      <div
+        class="fillter_view"
+        :class="{ fixed_class: bindFixed }"
+        :style="setTopStyle"
+      >
+        筛选区域
+      </div>
+      <ul class="list_area" title="列表区域">
+        <li v-for="(item, index) in list" :key="index" class="li_item">
+          {{ item }}
+        </li>
+      </ul>
+    </md-scroll-view>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HomeView",
+  data() {
+    return {
+      list: 50,
+      bindFixed: false,
+      scrollTop: 0,
+    };
+  },
+  computed: {
+    setTopStyle() {
+      return this.bindFixed ? { top: `${this.scrollTop}px` } : {};
+    },
+  },
+  methods: {
+    $_onScroll({ scrollLeft, scrollTop }) {
+      console.log(` ${scrollLeft}, scrollTop: ${scrollTop}`);
+      this.scrollTop = scrollTop;
+      if (scrollTop >= 150) {
+        this.bindFixed = true;
+      } else {
+        this.bindFixed = false;
+      }
+    },
+  },
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  height: 100vh;
+  box-sizing: border-box;
+  .head_desc {
+    height: 150px;
+    background-color: skyblue;
+  }
+  .fillter_view {
+    height: 80px;
+    background-color: rgb(171, 199, 47);
+  }
+  .li_item {
+    border-bottom: 1px solid #ccc;
+  }
+  .fixed_class {
+    position: absolute;
+    width: 100vw;
+  }
+}
+</style>
---
+++ src/views/transfer4.vue
@@ -0,0 +1,65 @@
+<!--
+ * @Descripttion:
+ * @version:
+ * @Author: CC
+ * @Date: 2022-05-24 15:52:49
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2022-08-23 16:19:22
+-->
+<template>
+  <div class="div1">
+    <div class="div2"></div>
+    <div class="div3"></div>
+    <div>
+      <ul>
+        <li class="liii" v-for="item in list" :key="item.index">{{ item.name }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  created() {
+    for (var i = 0; i < 30; i++) {
+      this.list.push({
+        index: i,
+        name: '张三' + i
+      })
+    }
+  },
+  data() {
+    return {
+      list: []
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style>
+.div1 {
+  height: 100vh;
+  position: relative;
+}
+.div2 {
+  width: 100%;
+  height: 30%;
+  background: skyblue;
+}
+.liii {
+  width: 100%;
+  height: 30px;
+  color: rgba(245, 122, 7, 0.726);
+  line-height: 30px;
+  font-size: 15px;
+}
+.div3 {
+  width: 100%;
+  height: 20px;
+  background: rgb(238, 17, 102);
+  position: sticky;
+  top: 0;
+}
+</style>
